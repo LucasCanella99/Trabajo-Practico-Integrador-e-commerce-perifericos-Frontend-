d8f2f909ebc3ce9ead70df80113d5e66d64e38e9
Zeke1ra <elias23sequeira@gmail.com>
2026-05-27 14:17:04 -0300

    update

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,4 +1,1 @@
 // aca va la interactividad de la pagina, como el carrito, el fecth de los productos, filtros, etc.document.getElementById("productos").scrollIntoView({
-document.getElementById("productos").scrollIntoView({
-  behavior: "smooth"
-});
